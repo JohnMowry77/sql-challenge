e4d57e8bbdfe078929826084fcfa63a755531b65
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-02-15 16:25:03 -0700

    Q 1 and 2 complete

--- maybe_delete.sql
+++ maybe_delete.sql
@@ -41,7 +41,7 @@ CREATE TABLE dept_emp (
 	dept_no VARCHAR   NOT NULL,    
 	PRIMARY KEY (emp_no, dept_no)
 );
---Dept_manager is a composite table where dept_no & emp_no are primary keys. emp_no goes to employee_table while dept_no goes to deptartments table
+
 CREATE TABLE "Dept_Manager" (
     "dept_no" varchar   NOT NULL,
     "emp_no" int   NOT NULL,
@@ -59,7 +59,7 @@ REFERENCES "Departments" ("dept_no");
 
 
 --Run each line to see the imported data
--- Need  assign as alias. Not required
+-- Need to assign as next. Not required
 SELECT * FROM "Departments"
 SELECT * FROM "Title"
 SELECT * FROM "Employees"
@@ -68,58 +68,12 @@ SELECT * FROM dept_emp
 SELECT * FROM "Dept_Manager"
 
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-select "Employees".emp_no as employee_number from "Employees"
 
-select "Employees".last_name as last_name from "Employees"
 
-select "Employees".first_name as first_name from "Employees"
-
-select "Employees".sex as sex from "Employees"
-
---Create a view of salary information with employee number:
-Create view salary as
---select "Employees".*, "Salaries".*
-select "Employees".emp_no, "Salaries".salary as salary
-from "Employees"
-join "Salaries"
-on "Salaries".emp_no="Employees".emp_no
-
---Save a view called employee_info will all the information requested:
-Create view employee_info as
-select "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Employees".sex, "Salaries".salary as salary
-from "Employees"
-join "Salaries"
-on "Salaries".emp_no="Employees".emp_no
-
---Since we saved the view we can call the alias:
-select * from employee_info
-select * from salary
- 
---drop salary view
-drop view salary;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 
---select "Employees".hire_date from "Employees";
-create view hire_year_1986 as
-Select "Employees".first_name, "Employees".last_name, "Employees".hire_date FROM "Employees"
---Select * from "Employees"
-WHERE hire_date BETWEEN '1986-01-01' and '1986-12-31';
-
-/* 3. List the manager of each department with the following information: 
-department number, department name, the manager's employee number, last name, first name.
-*/
-SELECT * FROM "Departments"
-
-SELECT "Departments".dept_no, "Departments".dept_name FROM "Departments"
-
-
-SELECT * FROM "Title"
-SELECT * FROM "Employees"
-SELECT * FROM "Salaries"
-SELECT * FROM dept_emp
-SELECT * FROM "Dept_Manager"
-
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
